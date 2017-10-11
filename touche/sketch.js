85b513b9d0466c68d5a6060ffd25abab29e69085
iothmani <inesothmani@Iness-MacBook-Pro.local>
2017-10-11 10:12:26 -0500

    Essai2

--- touche/sketch.js
+++ touche/sketch.js
@@ -12,15 +12,21 @@ function setup() {
 
 function draw(){
  background(255);
-fill(0);
+fill(255);
  for(var i=0;i<touches.length;i++){
   ellipse(touches[i].x,touches[i].y,80,80);
   }
-  fill(200,200,155);
+  fill(200,155,155);
    beginShape();
    
    for(var i=0;i<touches.length;i++){
   vertex(touches[i].x,touches[i].y);
   }
   endShape(CLOSE);
+}
+function touchStarted() {
+ background(255);
+ textSize(40);
+ text(touches[0].x, 100, 100);
+ text(touches[0].y, 200, 100);
 }
