45fc6b2fa59c1e44bedaa4f3273f70ecda3dddf6
iothmani <inesothmani@Iness-MacBook-Pro.local>
2017-10-11 09:48:04 -0500

    sc

--- touche/sketch.js
+++ touche/sketch.js
@@ -6,15 +6,14 @@ function setup() {
  createCanvas(windowWidth,windowHeight);
  rectMode(CENTER);
  stroke(0);
- fill(0);
+ noFill();
  strokeWeight(5);
 }
 
 function draw(){
  background(255);
- beginShape();
+ 
  for(var i=0;i<touches.length;i++){
-  vertex(touches[i].x,touches[i].y,150,150);
+  rect(touches[i].x,touches[i].y,150,150);
   }
-  endShape();
 }
