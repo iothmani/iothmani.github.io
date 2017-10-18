7f6fc1fd996895518e2420fe8e19a54af179138b
iothmani <inesothmani@Iness-MacBook-Pro.local>
2017-10-18 10:01:02 -0500

    b4

--- bille/sketch.js
+++ bille/sketch.js
@@ -3,21 +3,24 @@ var y=500;
 var v=0.1;
 var vx=0.01;
 var px=0;
+var ax=0.2;
 function setup() {
  createCanvas(windowWidth,windowHeight); 
 }
 
 function draw() {
   clear();
-
+fill(0);
  textSize(40);
  text("Rx: " + rotationX, 100, 100);
  text("Ry: " + rotationY, 100, 150);
  
  drawBalle();
+ 
+/**************************/
 
-  ax = y * v;
-  ay = x * v;
+ax = y * v;
+ay = x * v;
 vx += ax;
 px += vx;
 }
