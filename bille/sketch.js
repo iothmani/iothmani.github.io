b9d7fbc1eee72f7fa41530f30b2ff497004e8cfd
iothmani <inesothmani@Iness-MacBook-Pro.local>
2017-10-18 11:13:07 -0500

    Ver

--- bille/sketch.js
+++ bille/sketch.js
@@ -3,14 +3,18 @@ var posY;
 var multiplier;
 var vY, vX;
 var aX, aY;
+var Size;
+var bounce;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   vX = 0;
   vY = 0;
+bounce=0.5;
 
   posX = windowWidth / 2;
   posY = windowHeight / 2;
+  Size=60;
 }
 
 function draw() {
@@ -23,12 +27,19 @@ function draw() {
   textSize(40);
   text("Rx: " + rotationX, 100, 100);
   text("Ry: " + rotationY, 100, 150);
-  text("version3",100,200);
+  text("version4",100,200);
 
+  if((posY+Size/2)>=windowWidth)
+  {vX=-vX*bounce;
+  posX= windowWidth-Size/2;
+  }
   
+  else if((posY-Size/2<=0))
+  {vY=-vY*bounce
+  posY=Size/2}
 
   /**************************/
-  multiplier = 0.06;
+  multiplier = 0.01;
 
   aX = rotationY * multiplier;
   aY = rotationX * multiplier;
@@ -39,11 +50,12 @@ function draw() {
   posX += vX;
   posY += vY;
 
+
 }
 
 function drawBalle() {
   fill(60);
   noStroke();
-  ellipse(posX, posY, 60, 60);
+  ellipse(posX, posY, Size, 60);
 
 }
