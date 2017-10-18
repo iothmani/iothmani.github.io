a5b02ad93f5e5d3b522e701a8c0d6953f0428700
iothmani <inesothmani@Iness-MacBook-Pro.local>
2017-10-18 09:38:40 -0500

    b2

--- bille/sketch.js
+++ bille/sketch.js
@@ -1,11 +1,33 @@
+var x=0;
+var y=0;
+
 function setup() {
  createCanvas(windowWidth,windowHeight); 
 }
 
 function draw() {
- background(255);
+  clear();
+
  textSize(40);
  text("Rx: " + rotationX, 100, 100);
  text("Ry: " + rotationY, 100, 150);
- text("Rz: " + rotationZ, 100, 200);
+ 
+ drawBalle();
+ ballMove();
+}
+
+function drawBalle(){
+  fill(60);
+  noStroke();
+ellipse(x,y+ 5,60,60);
+  fill(150);
+  noStroke();
+ellipse(x,y,60,60);
+  fill(255);
+  noStroke();
+ellipse(x,y+ 10,20,20);
+}
+function ballMove(){
+  ax = deviceRotationY * vMultiplier;
+  ay = deviceRotationX * vMultiplier;
 }
