6d874686111324a78bc495d0afcc419fbef3dc3c
iothmani <inesothmani@Iness-MacBook-Pro.local>
2017-10-18 11:29:42 -0500

    Ver7

--- bille/sketch.js
+++ bille/sketch.js
@@ -27,25 +27,25 @@ function draw() {
   textSize(40);
   text("Rx: " + rotationX, 100, 100);
   text("Ry: " + rotationY, 100, 150);
-  text("version6",100,200);
+  text("version7",100,200);
 
   if((posY+Size/2)>=windowWidth)
   {vY=-vY*bounce;
   posX= windowWidth-Size/2;
   }
   
-  else if(posX-Size/2<=0)
+  else if((posX-Size/2)<=0)
   {vX=-vX*bounce
-  posX=Size/2}
+  posY=Size/2}
   
   if((posX+Size/2)>=windowHeight)
   {vX=-vX*bounce;
-  posY= windowHeight-Size/2;
+  posY= windowWidth-Size/2;
   }
   
-  else if(posY-Size/2<=0)
+  else if((posY-Size/2)<=0)
   {vY=-vY*bounce
-  posY=Size/2}
+  posX=Size/2}
   
   /**************************/
   multiplier = 0.01;
