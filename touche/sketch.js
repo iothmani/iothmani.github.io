2cc4aec1c951fe006f470011da2a740c8096bd93
iothmani <inesothmani@Iness-MacBook-Pro.local>
2017-11-01 10:29:15 -0500

    correc

--- touche/sketch.js
+++ touche/sketch.js
@@ -12,7 +12,7 @@ function setup() {
 
 function draw(){
  background(255);
-  fill(200,255,155);
+  fill(255,235,155);
   
  for(var i=0;i<touches.length;i++){
   ellipse(touches[i].x,touches[i].y,80,80);
@@ -26,8 +26,8 @@ function draw(){
    
    for(var i=0;i<touches.length;i++){
   vertex(touches[i].x,touches[i].y);
-   text("x="+ floor (touches[i].x),touches[i].x + 20,touches[i].y);
-    text("y="+ floor (touches[i].y),touches[i].x ,touches[i].y+ 20);
+   text("x="+ floor (touches[i].x),touches[i].x + 30,touches[i].y);
+    text("y="+ floor (touches[i].y),touches[i].x + 30,touches[i].y+ 40);
 
   }
   endShape(CLOSE);
