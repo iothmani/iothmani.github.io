8bd128f7c59da411e31686045aa8d1b5849e4af8
iothmani <inesothmani@Iness-MacBook-Pro.local>
2017-11-01 10:25:36 -0500

    Poor

--- touche/sketch.js
+++ touche/sketch.js
@@ -12,22 +12,23 @@ function setup() {
 
 function draw(){
  background(255);
- fill(255);
- textSize(40);
-
+  fill(200,255,155);
+  
  for(var i=0;i<touches.length;i++){
   ellipse(touches[i].x,touches[i].y,80,80);
-
   }
-  fill(200,155,155);
+  
+  fill(0);
   noStroke();
+ textSize(40);
+ 
    beginShape();
    
    for(var i=0;i<touches.length;i++){
   vertex(touches[i].x,touches[i].y);
-  
    text("x="+ floor (touches[i].x),touches[i].x + 20,touches[i].y);
-    text("y="+ floor (touches[i].y),touches[i].x + 20,touches[i].y);
+    text("y="+ floor (touches[i].y),touches[i].x ,touches[i].y+ 20);
+
   }
   endShape(CLOSE);
 
